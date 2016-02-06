59579b331103832fdbe4f1327ac12316e4731361
TheMiken16 <omarskate08@gmail.com>
2016-02-06 18:42:18 -0500

    Añadida migraciones, models productos y creado superuser

--- Shoppy/settings.py
+++ Shoppy/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'productos.apps.ProductosConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
